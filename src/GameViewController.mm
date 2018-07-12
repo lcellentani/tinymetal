07e92c22054edfd0e15b872fca082e08a2c31877
ludo <lcellentani@gmail.com>
2018-07-12 18:43:33 +0200

    Project setup

---
+++ src/GameViewController.mm
@@ -0,0 +1,24 @@
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController {
+}
+- (instancetype)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+
+@end
